6a84184dde82146725b7e91a446e0944af35ca97
Forrest Fuqua <kingjrwr@gmail.com>
2017-07-19 10:24:28 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-echo "Test Run Completed"
+reset
+echo "=== Starting Warrior Download ==="
+docker run -d -p 8001:8001 archiveteam/warrior-dockerfile &>/tmp/dockerid
+DOCKERID=`cat /tmp/dockerid`
+echo $dockerid
+reset
+echo "================================="
+echo "You can now login to the web interface at
+echo " http://127.0.0.1:8001"
 while true; do
 sleep 30
 done
