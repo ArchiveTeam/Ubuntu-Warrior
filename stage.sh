4438fd062b921156bd6744bcf633fea60797626f
Forrest Fuqua <kingjrwr@gmail.com>
2017-07-19 15:06:41 -0500

    Create stage.sh

---
+++ stage.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#This script is to setup a default install of Alpine Linux installed to disk.
+# The Following Settings where used in the install
+# ISO: alpine-virt-3.6.2-x86_64.iso
+# Disk: Blank
+# Keyboard: us | Variant: us
+# Hostname: warrior
+# Network: eth0 dhcp no manual config
+# Root Password: warrior
+# UTC Timezone
+# No HTTP Proxy
+# Mirror: dl-2.alpinelinux.org
+# SSH Server: None
+# NTP Client: chrony
+# Disk to use: sda | Yes Erase | Use it: sys
+# =REBOOT=
+# apk add openssl
+# wget 
