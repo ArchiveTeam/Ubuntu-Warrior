327c9683f553b42b988193adb41fb42bd2858931
Forrest Fuqua <kingjrwr@gmail.com>
2017-07-19 10:55:44 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ reset
 echo "=== Starting Warrior Download ==="
 docker run -d -p 8001:8001 archiveteam/warrior-dockerfile
 reset
-if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ] then
+if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ]; then
 echo "Startup Failure! Unable to start the Docker Instance, Sleeping 30 Seconds"
 sleep 30
 exit
@@ -13,4 +13,8 @@ echo "You can now login to the web interface at"
 echo " http://127.0.0.1:8001"
 while true; do
 sleep 10
+if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ]; then
+echo "Docker Detected Instance has been turned OFF, Powering off!"
+poweroff
+fi
 done
