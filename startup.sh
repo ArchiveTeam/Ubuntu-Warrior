76af3cc1235b5f2d13afe7e441ffd5b4ef42bde9
Forrest Fuqua <kingjrwr@gmail.com>
2017-07-19 10:48:54 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ echo "=== Starting Warrior Download ==="
 docker run -d -p 8001:8001 archiveteam/warrior-dockerfile
 reset
 if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ] then
-echo "Startup Failure! Unable to start the Docker Instance, Sleeping 30 Seconds
+echo "Startup Failure! Unable to start the Docker Instance, Sleeping 30 Seconds"
 sleep 30
 exit
 fi
-echo "================================="
+echo "=== Archive Team Warrior ==="
 echo "You can now login to the web interface at"
 echo " http://127.0.0.1:8001"
 while true; do
