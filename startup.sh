068655abfe17d2e4dd46895e63a97816519fa10b
Christopher Foo <chris.foo@gmail.com>
2017-10-09 22:21:59 -0400

    startup.sh: Add a way for the warrior to tell host to reboot or shutdown

--- startup.sh
+++ startup.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 reset
 echo "=== Starting Warrior Download ==="
-docker run -d -p 8001:8001 archiveteam/warrior-dockerfile
+docker run -d -p 8001:8001 archiveteam/warrior-dockerfile \
+    --cidfile "/root/docker_container_id.txt"
+
 reset
 if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ]; then
 echo "Startup Failure! Unable to start the Docker Instance, Sleeping 30 Seconds"
 sleep 30
 exit
 fi
+
 echo
 echo "=== Archive Team Warrior ==="
 echo
@@ -18,10 +21,22 @@ echo "and login to the web interface at"
 echo " http://127.0.0.1:8001"
 echo
 sleep 120
+
+CONTAINER_ID = `cat /root/docker_container_id.txt`
+
 while true; do
 sleep 10
-if [ ! "$(docker ps -a | grep archiveteam/warrior-dockerfile)" ]; then
-echo "Docker Detected Instance has been turned OFF, Powering off!"
-poweroff
+if docker exec -it $CONTAINER_ID test -f /tmp/warrior_reboot_required; then
+    echo "Detected warrior needing reboot. Rebooting!"
+    reboot
+elif docker exec -it $CONTAINER_ID test -f /tmp/warrior_poweroff_required; then
+    echo "Detected warrior needing poweroff. Powering off!"
+    poweroff
+elif docker ps -f id=$CONTAINER_ID -f status=dead | grep $CONTAINER_ID; then
+    echo "Docker container instance dead. Rebooting!"
+    reboot
+elif docker ps -f id=$CONTAINER_ID -f status=exited | grep $CONTAINER_ID; then
+    echo "Docker container instance exited, Powering off!"
+    poweroff
 fi
 done
