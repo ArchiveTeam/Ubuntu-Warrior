b93bbe16667f40d6671a86d48a7c7d4302e5cf1f
tech234a <46801700+tech234a@users.noreply.github.com>
2021-02-23 18:02:48 -0500

    Fix mount permissions

--- startup.sh
+++ startup.sh
@@ -27,7 +27,9 @@ fi
 if [ ! "$(docker ps -a --format {{.Names}} | grep warrior)" ]; then
     echo "Please wait while the Warrior is downloaded and started..."
     echo "This may take a few minutes..."
-    docker run -d -p 8001:8001 --name warrior --mount type=bind,source=/root/config.json,target=/home/warrior/projects/config.json atdr.meo.ws/archiveteam/warrior-dockerfile
+    # Mount the user configuration from the host container
+    # https://stackoverflow.com/a/54787364, https://docs.docker.com/storage/bind-mounts
+    docker run -d -p 8001:8001 --name warrior -v/root/config.json:/home/warrior/projects/config.json:z atdr.meo.ws/archiveteam/warrior-dockerfile
     # Allow reading network stats by non-root
     docker exec -it warrior adduser warrior dip
 else
