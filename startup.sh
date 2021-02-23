fdd5ce8b2ed7adfc7b31499ab36fe2cd425ffad1
tech234a <46801700+tech234a@users.noreply.github.com>
2021-02-23 17:30:15 -0500

    Migrate to new Warrior image, other enhancements
    This change detects the old Warrior image, backs up the user config.json, deletes the container, and then prunes everything from Docker. If the new image or the Watchtower image aren't found, they are installed and configured. The user configuration is now stored on the host system and bind mounted into the Docker image so it will survive container updates as well as the complete deletion of the container. Additionally, container names are used instead of container IDs when referring to containers since Watchtower doesn't preserve container IDs on automatic updates.

--- startup.sh
+++ startup.sh
@@ -2,18 +2,39 @@
 reset
 echo "=== Starting Warrior Download ==="
 if [ -f /root/docker_container_id.txt ]; then
+    echo "A new, upgraded Warrior capable of running more projects is available and will now be installed..."
     CONTAINER_ID=`cat /root/docker_container_id.txt`
-    docker start $CONTAINER_ID
-else
-    docker run -d -p 8001:8001 --cidfile="/root/docker_container_id.txt" \
-        archiveteam/warrior-dockerfile
-    CONTAINER_ID=`cat /root/docker_container_id.txt`
-
-    # Allow reading network stats by non-root
-    docker exec -it $CONTAINER_ID adduser warrior dip
+    echo "Backing up the user configuration..."
+    docker cp $CONTAINER_ID:/home/warrior/projects/config.json /root/config.json
+    echo "Cleaning up the old version..."
+    docker rm $CONTAINER_ID
+    docker system prune -a -f --volumes
+    rm /root/docker_container_id.txt
+    echo "Now ready to install the new Warrior and automatic updater!"
 fi
 
-if [ ! "$(docker ps | grep archiveteam/warrior-dockerfile)" ]; then
+# Create a blank configuration file if none exists, otherwise do nothing
+touch /root/config.json # https://unix.stackexchange.com/a/343558
+
+# https://stackoverflow.com/a/50667460
+if [ ! "$(docker ps -a --format {{.Names}} | grep watchtower)" ]; then
+    echo "Please wait while the automatic updater is prepared..."
+    docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+else
+    docker start watchtower
+fi
+
+if [ ! "$(docker ps -a --format {{.Names}} | grep warrior)" ]; then
+    echo "Please wait while the Warrior is downloaded and started..."
+    echo "This may take a few minutes..."
+    docker run -d -p 8001:8001 --name warrior -v /root/config.json:/home/warrior/projects/config.json atdr.meo.ws/archiveteam/warrior-dockerfile
+    # Allow reading network stats by non-root
+    docker exec -it warrior adduser warrior dip
+else
+    docker start warrior
+fi
+
+if [ ! "$(docker ps -a --format {{.Names}} | grep warrior)" ]; then
 echo "***** Startup Failure! ******"
 echo "Unable to start the Docker Instance"
 echo "Sleeping 30 seconds before retrying..."
@@ -21,9 +42,7 @@ sleep 30
 exit 1
 fi
 
-CONTAINER_ID=`cat /root/docker_container_id.txt`
-
-docker exec -it $CONTAINER_ID rm -f /tmp/warrior_reboot_required \
+docker exec -it warrior rm -f /tmp/warrior_reboot_required \
     /tmp/warrior_poweroff_required
 
 echo "Warrior is updating Seesaw Kit."
@@ -32,7 +51,7 @@ echo "Please wait..."
 
 for i in `seq 60`; do
 sleep 5
-if docker top $CONTAINER_ID | grep run-warrior; then
+if docker top warrior | grep run-warrior; then
     break
 elif [ $i -eq 60 ]; then
     echo "***** Startup Failure! ******"
@@ -61,16 +80,16 @@ sleep 20
 
 while true; do
 sleep 10
-if docker exec -it $CONTAINER_ID test -f /tmp/warrior_reboot_required; then
+if docker exec -it warrior test -f /tmp/warrior_reboot_required; then
     echo "Detected warrior needing reboot. Rebooting!"
     reboot
-elif docker exec -it $CONTAINER_ID test -f /tmp/warrior_poweroff_required; then
+elif docker exec -it warrior test -f /tmp/warrior_poweroff_required; then
     echo "Detected warrior needing poweroff. Powering off!"
     poweroff
-elif docker ps -f id=$CONTAINER_ID -f status=dead | grep $CONTAINER_ID; then
+elif docker ps -f name=warrior -f status=dead | grep warrior; then
     echo "Docker container instance dead. Rebooting!"
     reboot
-elif docker ps -f id=$CONTAINER_ID -f status=exited | grep $CONTAINER_ID; then
+elif docker ps -f name=warrior -f status=exited | grep warrior; then
     echo "Docker container instance exited, Powering off!"
     poweroff
 fi
