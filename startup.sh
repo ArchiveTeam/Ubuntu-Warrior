a517f6d375fb7fcd4fe3b53048e8478fcfbf4e25
tech234a <46801700+tech234a@users.noreply.github.com>
2021-02-24 23:48:30 -0500

    Add comments documenting new sections

--- startup.sh
+++ startup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 reset
 echo "=== Starting Warrior Download ==="
+
+# Versions 3.0 and 3.1 of the VM image use a file at /root/docker_container_id.txt to keep track
+# of the container ID for the Warrior image. If this file is detected, the user is migrated to
+# the new Warrior container. The migration process consists of extracting the user configuration
+# file from the old container and then deleting the old container, pruning Docker containers,
+# networks, and images (the --volumes parameter is not supported on the Docker version in the
+# 3.0 VM image), and deleting the /root/docker_container_id.txt file.
+# https://docs.docker.com/engine/reference/commandline/system_prune/
 if [ -f /root/docker_container_id.txt ]; then
     echo "A new, upgraded Warrior capable of running more projects is available and will now be installed..."
     CONTAINER_ID=`cat /root/docker_container_id.txt`
@@ -19,6 +27,8 @@ touch /root/config.json # https://unix.stackexchange.com/a/343558
 # Make sure the container has access to the config file
 chmod 777 /root/config.json
 
+# If a container named watchtower does not exist, create and configure it.
+# Watchtower is configured to check for updates every hour, and to delete outdated images.
 # https://stackoverflow.com/a/50667460
 if [ ! "$(docker ps -a --format {{.Names}} | grep watchtower)" ]; then
     echo "Please wait while the automatic updater is prepared..."
@@ -27,6 +37,12 @@ else
     docker start watchtower
 fi
 
+# If a container named warrior does not exist, create and configure it.
+# Previous versions of startup.sh did not name containers.
+# Additionally, the /root/config.json file is mounted inside the Docker container at
+# /home/warrior/projects/config.json, allowing user configuration to be persisted across
+# container deletions and Watchtower updates.
+# https://stackoverflow.com/a/50667460
 if [ ! "$(docker ps -a --format {{.Names}} | grep warrior)" ]; then
     echo "Please wait while the Warrior is downloaded and started..."
     echo "This may take a few minutes..."
