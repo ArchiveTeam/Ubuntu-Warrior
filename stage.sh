0d26aee8f3cabd8f9da8c79e6fab57907f617d84
tech234a <46801700+tech234a@users.noreply.github.com>
2021-02-26 18:10:26 -0500

    Download splashes

--- stage.sh
+++ stage.sh
@@ -72,6 +72,15 @@ set > /root/env.sh
 wget ${REPO_PREFIX}${BRANCH}/boot.sh -O /root/boot.sh
 chmod +x /root/boot.sh
 
+# Download splashes
+mkdir /root/splashes
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-ready-640x400-32.fb -O /root/splashes/at-splash-ready-640x400-32.fb
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-update-640x400-32.fb -O /root/splashes/at-splash-update-640x400-32.fb
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-outdated-640x400-32.fb -O /root/splashes/at-splash-outdated-640x400-32.fb
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-startup-640x400-32.fb -O /root/splashes/at-splash-startup-640x400-32.fb
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-shutdown-640x400-32.fb -O /root/splashes/at-splash-shutdown-640x400-32.fb
+wget ${REPO_PREFIX}${BRANCH}/splashes/at-splash-restart-640x400-32.fb -O /root/splashes/at-splash-restart-640x400-32.fb
+
 # Update, install and configure framebuffer
 apk update
 apk add v86d
