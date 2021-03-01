d44e4a2589080adb1240a0153645cea03882d2ae
tech234a <46801700+tech234a@users.noreply.github.com>
2021-03-01 00:15:24 -0500

    Alpine Linux 3.13.2

--- build-vm.sh
+++ build-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VMNAME="archiveteam-warrior-3.2-beta"
-INSTALL_ISO="alpine-virt-3.12.0-x86_64.iso"
+INSTALL_ISO="alpine-virt-3.13.2-x86_64.iso"
 
 VBoxManage createvm --name $VMNAME --ostype Linux_64 --register
 VBoxManage modifyvm $VMNAME \
